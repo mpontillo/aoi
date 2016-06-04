0b2d5554128f124602c1c64bc8e0aab0f366ecd1
Mike Pontillo <pontillo@gmail.com>
2016-06-03 17:51:24 -0700

    Add the ability to specify a default value when getting a configuration key.

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
 import os
 import sys
-from pprint import pprint
 
 def write_config(config_file, config):
     config_json = json.dumps(config)
@@ -12,34 +12,93 @@ def write_config(config_file, config):
         f.write("\n")
     aoi_config = {}
 
-def get_config_filename():
+def get_config_dir():
     home = os.path.expanduser('~')
-    config_file = home + "/.aoi/config"
+    config_dir = home + "/.aoi"
+    return config_dir
+
+def get_config_filename():
+    config_file = get_config_dir() + "/config"
     return config_file
 
 def read_or_create_config():
+    config_dir = get_config_dir()
+    if not os.path.isdir(config_dir):
+        os.makedirs(config_dir)
     config_file = get_config_filename()
     if os.path.isfile(config_file):
         with open(config_file, 'r') as f:
             aoi_config = json.loads(f.read())
     else:
         # Create the initial configuraton file.
-        write_config(config_file, {})
+        aoi_config = {}
+        write_config(config_file, aoi_config)
     return aoi_config
 
-def main(args):
+def show_command():
     aoi_config = read_or_create_config()
-    if len(args) == 0 or args[0] == 'show':
-        config_json = json.dumps(aoi_config, indent=4, sort_keys=True)
-        print(config_json)
-    elif args[0] == 'get':
-        print(aoi_config[args[1]])
-    elif args[0] == 'set':
-        aoi_config[args[1]] = args[2]
-        write_config(get_config_filename(), aoi_config)
-    elif args[0] == 'del' or args[0] == 'delete':
-        del aoi_config[args[1]]
-        write_config(get_config_filename(), aoi_config)
+    config_json = json.dumps(aoi_config, indent=4, sort_keys=True)
+    print(config_json)
+
+def get_command(key, default=None):
+    aoi_config = read_or_create_config()
+    if default is not None and key not in aoi_config:
+        print(default)
+    else:
+        print(aoi_config[key])
+
+def set_command(key, value):
+    aoi_config = read_or_create_config()
+    aoi_config[key] = value
+    write_config(get_config_filename(), aoi_config)
+
+def delete_command(key):
+    aoi_config = read_or_create_config()
+    del aoi_config[key]
+    write_config(get_config_filename(), aoi_config)
+
+def configure_subparser(parser):
+    subparsers = parser.add_subparsers()
+    # show
+    show_parser = subparsers.add_parser(
+        'show', aliases=['read', 'print'],
+        help="Print the configuration (in JSON form).")
+    show_parser.set_defaults(func=show_command)
+    # set <key> <value>
+    set_parser = subparsers.add_parser(
+        'set', help="Set the configuration for the specified key.")
+    set_parser.add_argument(
+        'key', type=str, help="Key name to set.")
+    set_parser.add_argument(
+        'value', type=str, help="Value to set.")
+    set_parser.set_defaults(func=set_command)
+    # get <key> [default]
+    get_parser = subparsers.add_parser(
+        'get', help="Get the configuration for the specified key.")
+    get_parser.set_defaults(func=get_command)
+    get_parser.add_argument('key', type=str, help="Key name to get.")
+    get_parser.add_argument(
+        'default', nargs='?', default=None, type=str,
+        help="Default value (optional).")
+    get_parser.set_defaults(func=get_command)
+    # delete <key>
+    delete_parser = subparsers.add_parser(
+        'delete', aliases=['del', 'rm'], help="Delete the specified key.")
+    delete_parser.add_argument('key', type=str, help="Key name to delete.")
+    delete_parser.set_defaults(func=delete_command)
+
+def main():
+    parser = argparse.ArgumentParser(prog="config.py")
+    configure_subparser(parser)
+    parser.set_defaults(func=show_command)
+    out = parser.parse_args(sys.argv[1:])
+    kwargs = vars(out)
+    func = kwargs.get('func', None)
+    if func is not None:
+        del kwargs['func']
+        func(**vars(out))
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
